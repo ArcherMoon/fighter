dc9db7ef04c10657b215baeec67e34ac3c46d4c5
ArcherMoon <zjhn1986@gmail.com>
2013-05-20 23:42:38 +0800

    补充其他7个方向键

--- SimpleDPad.cpp
+++ SimpleDPad.cpp
@@ -80,8 +80,39 @@ void SimpleDPad::updateDirectionForTouchLocation(cocos2d::CCPoint location)
         /* right */
         _direction = ccp(1.0, 0.0);  
     }
-    else
+    else if (degrees > 22.5 && degrees < 67.5)
     {
+        /* bottomright  */
+        _direction = ccp(1.0, -1.0);  
+    }
+    else if (degrees >= 67.5 && degrees <= 112.5)
+    {
+        /* bottom */
+        _direction = ccp(0.0, -1.0);  
+    }
+    else if (degrees > 112.5 && degrees <157.5)
+    {
+        /* bottomleft */
+        _direction = ccp(-1.0, -1.0);  
+    }
+     else if (degrees >= 157.5 || degrees <= -157.5)
+    {
+        /* left */
+        _direction = ccp(-1.0, 0.0);  
+    }
+    else if (degrees > -157.5 && degrees < -112.5)
+    {
+        /* topleft */
+        _direction = ccp(-1.0, 1.0);  
+    }
+    else if (degrees >= -112.5 && degrees <= -67.5)
+    {
+        /* top */
+        _direction = ccp(0.0, 1.0);  
+    }
+    else if (degrees > -67.5 && degrees < -22.5)
+    {
+        /* topright */
         _direction = ccp(1.0, 1.0);  
     }
 
